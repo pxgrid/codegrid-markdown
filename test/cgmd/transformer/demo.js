1d7a09de696bff8fe929cc5cb11f06aacefb2022
leader22 <y.sugiura.0316@gmail.com>
2016-04-22 16:04:31 +0900

    使われてないicon-toolを消す

--- test/cgmd/transformer/demo.js
+++ test/cgmd/transformer/demo.js
@@ -8,13 +8,13 @@ describe('CodeGridMarkdown - Transformer - Demo', function() {
 describe('#demo', function() {
   it('レンダリングされること', function() {
     var res = Transformer.transform('<div class="Demo">\n<h1>タイトル</h1>\n<iframe data-src="http://example.com" data-deferred="true"></iframe>\n</div>\n');
-    var expect = '<div class="CG2-livecode" data-livecode-deferred=""><header class="CG2-livecode__header"><div class="CG2-livecode__label">タイトル</div><div class="CG2-livecode__nav"><ul><li><a href="http://example.com"><span class="CG2-icon-tool"></span> 新規タブで開く</a></li></ul></div></header><div class="CG2-livecode__body"><iframe data-src="http://example.com" data-deferred="true"></iframe></div></div>\n';
+    var expect = '<div class="CG2-livecode" data-livecode-deferred=""><header class="CG2-livecode__header"><div class="CG2-livecode__label">タイトル</div><div class="CG2-livecode__nav"><ul><li><a href="http://example.com">新規タブで開く</a></li></ul></div></header><div class="CG2-livecode__body"><iframe data-src="http://example.com" data-deferred="true"></iframe></div></div>\n';
     assert.equal(res, expect);
   });
 
   it('レンダリングされること そのままパターン', function() {
     var res = Transformer.transform('<div class="Demo">\n<h1>タイトル</h1>\n<iframe src="http://example.com"></iframe>\n</div>\n');
-    var expect = '<div class="CG2-livecode"><header class="CG2-livecode__header"><div class="CG2-livecode__label">タイトル</div><div class="CG2-livecode__nav"><ul><li><a href="http://example.com"><span class="CG2-icon-tool"></span> 新規タブで開く</a></li></ul></div></header><div class="CG2-livecode__body"><iframe src="http://example.com"></iframe></div></div>\n';
+    var expect = '<div class="CG2-livecode"><header class="CG2-livecode__header"><div class="CG2-livecode__label">タイトル</div><div class="CG2-livecode__nav"><ul><li><a href="http://example.com">新規タブで開く</a></li></ul></div></header><div class="CG2-livecode__body"><iframe src="http://example.com"></iframe></div></div>\n';
     assert.equal(res, expect);
   });
 });
