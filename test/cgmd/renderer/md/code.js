b0b06f7f2ad4fc3182d4d41760d2518bd9610ad5
Yuji Sugiura <y.sugiura.0316@gmail.com>
2021-02-19 15:21:35 +0900

    Remove local diff

--- test/cgmd/renderer/md/code.js
+++ test/cgmd/renderer/md/code.js
@@ -23,19 +23,6 @@ describe('#code', function() {
     assert.equal(html, expect);
   });
   it('言語とタイトル指定が正しい場合は拡張したやつ', function() {
-    var html1 = renderer.render('```#title\nhoge\n```');
-    var expect = '' +
-      '<section class="CG2-livecode">\n' +
-        '<header class="CG2-livecode__header">\n' +
-          '<div class="CG2-livecode__label">title</div>\n' +
-        '</header>\n' +
-        '<div class="CG2-livecode__body">' +
-          '<pre><code>hoge</code></pre>\n' +
-        '</div>\n' +
-      '</section>\n';
-    assert.equal(html1, expect);
-  });
-  it('言語とタイトル指定が正しい場合は拡張したやつ', function() {
     var html1 = renderer.render('```html#title\nhoge\n```');
     // スペースがあっても気にしない
     var html2 = renderer.render('```html #title\nhoge\n```');
