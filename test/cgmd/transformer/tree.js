92c5f3086fc413a29105356a235f5611972abb66
Masataka Yakura <yakura@pxgrid.com>
2025-09-24 18:37:19 +0900

    treeのテストを修正

--- test/cgmd/transformer/tree.js
+++ test/cgmd/transformer/tree.js
@@ -18,7 +18,7 @@ describe("CodeGridMarkdown - Transformer - Tree", function () {
         '<div class="Tree"><h3>見出し</h3><ul><li>directory/<ul><li>file1.ext</li></ul></li><li>file2.ext</li><li>a-lot-of-files/</li></ul></div>'
       );
       var expect =
-        '<div class="Tree"><h3>見出し</h3><ul><li class="directory"><details open="true"><summary>directory/</summary><ul><li data-file-type="ext" class="file">file1.ext</li></ul></details></li><li data-file-type="ext" class="file">file2.ext</li><li class="directory"><details><summary>a-lot-of-files/</summary><ul><li class="file">...</li></ul></details></li></ul></div^';
+        '<div class="Tree"><h3>見出し</h3><ul><li class="directory"><details open="true"><summary>directory/</summary><ul><li data-file-type="ext" class="file">file1.ext</li></ul></details></li><li data-file-type="ext" class="file">file2.ext</li><li class="directory"><details><summary>a-lot-of-files/</summary><ul><li class="file">...</li></ul></details></li></ul></div>';
       assert.equal(res, expect);
     });
   });
