ae01ab103edcbc4e6245b57d230a6058eec155e9
Masataka Yakura <yakura@pxgrid.com>
2025-09-26 17:41:08 +0900

    `__dirname` → `import.meta.dirname`

--- example/main.js
+++ example/main.js
@@ -4,7 +4,7 @@ import CodeGridMarkdown from '../lib/index.js';
 const CGMDRenderer = new CodeGridMarkdown({
   // options for marked
 });
-const str = fs.readFileSync(__dirname + '/cg.md', 'utf-8');
+const str = fs.readFileSync(import.meta.dirname + '/cg.md', 'utf-8');
 
 const htmlStr = CGMDRenderer.render(str);
 console.log(htmlStr);
